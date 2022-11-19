d88a519c43205ff64af5d3a0b2093dbd66a63f82
Jaap de Haan <ColorOfCode@googlemail.com>
2022-11-19 15:20:01 +0100

    fix: eslint configuration

--- multiplication/src/index.tsx
+++ multiplication/src/index.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from 'react-dom';
 import './assets/index.scss';
 
-const Application: React.FunctionComponent<{}> = () => (
-  <h1>Application</h1>
+const Application: React.FunctionComponent = () => (
+    <h1>Application</h1>
 );
 
 render(<Application />, document.getElementById('root'));
