6a80f50075d47813268c8ef3c90f38e57bd356d4
Jaap de Haan <ColorOfCode@googlemail.com>
2022-11-27 17:40:45 +0100

    fix: Cleanup interpolate into lib

--- multiplication/src/component/statistics.tsx
+++ multiplication/src/component/statistics.tsx
@@ -1,26 +1,11 @@
 import { min, range } from 'lodash';
 import React, { ReactNode } from 'react';
 
+import { interpolateColor } from '../utils';
 import { Statistics } from '../types';
 
 interface Props {
   statistics: Statistics
-}
-
-function hex(input: number): string {
-  const x = input.toString(16);
-  return (x.length === 1) ? '0' + x : x;
-}
-
-function getValue(input: string, position: number): number {
-  return parseInt(input.substring(position, position + 2), 16);
-}
-
-function interpolateColor(color1: string, color2: string, ratio: number): string {
-  const r = Math.ceil(getValue(color1, 0) * ratio + getValue(color2, 0) * (1 - ratio));
-  const g = Math.ceil(getValue(color1, 2) * ratio + getValue(color2, 2) * (1 - ratio));
-  const b = Math.ceil(getValue(color1, 4) * ratio + getValue(color2, 4) * (1 - ratio));
-  return '#' + hex(r) + hex(g) + hex(b);
 }
 
 const StatisticsView: React.FC<Props> = ({ statistics }) => {
--- multiplication/src/utils/index.ts
+++ multiplication/src/utils/index.ts
@@ -13,7 +13,7 @@ function createQuestion (): Question {
   const correct = `${a * b}`;
   const incorrect = uniq(
     [`${a * b - 2}`, `${a * b + 2}`, `${a * (b - 1)}`, `${a * (b + 1)}`].filter(
-      (n) => +n > 0 && +n < 100 && +n !== +correct
+      n => +n > 0 && +n < 100 && +n !== +correct
     )
   );
   return {
@@ -25,3 +25,31 @@ function createQuestion (): Question {
 
 export const createQuestions = (amount: number): Question[] =>
   [...Array(amount).keys()].map(() => createQuestion());
+
+function hex (input: number): string {
+  const x = input.toString(16);
+  return x.length === 1 ? '0' + x : x;
+}
+
+function getValue (input: string, position: number): number {
+  return parseInt(input.substring(position, position + 2), 16);
+}
+
+function interpolateValue (
+  value1: number,
+  value2: number,
+  ratio: number
+): number {
+  return Math.ceil(value1 * ratio + value2 * (1 - ratio));
+}
+
+export function interpolateColor (
+  color1: string,
+  color2: string,
+  ratio: number
+): string {
+  const rgb = [0, 2, 4].map(x =>
+    interpolateValue(getValue(color1, x), getValue(color2, x), ratio)
+  );
+  return '#' + rgb.map(hex).join('');
+}
